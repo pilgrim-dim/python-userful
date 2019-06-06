f6be92a0aa7c5e4a67700ae4460f667b0d39ef57
blind-eye-brainy-brain <34907113+blind-eye-brainy-brain@users.noreply.github.com>
2019-06-06 14:52:44 +0900

    Update altert_utils.py
    remove personal information

--- scrap/alert_utils.py
+++ scrap/alert_utils.py
@@ -29,7 +29,7 @@ send_email(
     '[work_id_collect] program has been finished',
     'program has been finished at %s' % (datetime.datetime.now()),
     'alert.by.speechless',
-    '!skrmsptjdwjs9003',
-    'hellossong@gmail.com',
+    'password',
+    'email@gmail.com',
 )
 '''
